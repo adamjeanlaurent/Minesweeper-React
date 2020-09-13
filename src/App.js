62e2cd36ec9620b41d81af5d36142a230a00fc62
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-09-12 20:49:35 -0400

    game finished

--- src/App.js
+++ src/App.js
@@ -1,69 +1,91 @@
-import React, {useState} from 'react';
-import './App.css';
-import uniqid from 'uniqid';
+import React, { useState } from "react";
+import "./App.css";
+import uniqid from "uniqid";
 
-import Board from './lib/Board';
-import { logRoles } from '@testing-library/react';
+import Board from "./lib/Board";
+import { logRoles } from "@testing-library/react";
 
 function App() {
-  
-  // setup game board
-  let board = new Board();
-  board.setup();
-  
-  function squareClicked(e) {
-    const x = parseInt(e.target.getAttribute('x'));
-    const y = parseInt(e.target.getAttribute('y'));
-    const text = e.target.getAttribute('text');
+    // setup game board
+    let board = new Board();
+    board.setup();
 
-    let squareThatWasClicked = board.matrix[x][y];
+    let mouseControlsEnabled = true;
 
-    if(!squareThatWasClicked.isVisible) {
-      squareThatWasClicked.isVisible = true;
-      if(squareThatWasClicked.isBomb) {
-        e.target.textContent = text;
-        e.target.style.backgroundColor = 'lightblue';
-        board.lose();
-      }
-      else {
-        board.floodFillReveal(x,y);
-      }
-    }
-  }
+    function squareClicked(e) {
+      if(!mouseControlsEnabled) return;
+        const x = parseInt(e.target.getAttribute("x"));
+        const y = parseInt(e.target.getAttribute("y"));
+        const text = e.target.getAttribute("text");
 
-    document.addEventListener('contextmenu', (e) =>  {
-      const flag = '🚩';
-      e.preventDefault();
-      if(e.target.classList !== 0) {
-        if(e.target.classList.contains('square')) {
-          const x = parseInt(e.target.getAttribute('x'));
-          const y = parseInt(e.target.getAttribute('y'));
-          
-          if(!board.matrix[x][y].visible) {
-            if(e.target.textContent === flag) {
-              e.target.textContent = '';
+        let squareThatWasClicked = board.matrix[x][y];
+
+        if (!squareThatWasClicked.isVisible) {
+            squareThatWasClicked.isVisible = true;
+            if (squareThatWasClicked.isBomb) {
+                e.target.textContent = text;
+                e.target.style.backgroundColor = "lightblue";
+                lose();
+            } else {
+                board.floodFillReveal(x, y);
             }
-            else {
-              e.target.textContent = flag;
-            }
-          }
         }
-      }
+    }
+
+    function lose() {
+        mouseControlsEnabled = false;
+        for (let i = 0; i < board.rows; i++) {
+            for (let j = 0; j < board.cols; j++) {
+                let square = board.matrix[i][j];
+                if (!square.visible) {
+                    let squareDOM = document.getElementById("" + i + j);
+                    squareDOM.textContent = square.text;
+                    squareDOM.style.backgroundColor = "lightblue";
+                }
+            }
+        }
+    }
+
+    document.addEventListener("contextmenu", (e) => {
+        const flag = "🚩";
+        e.preventDefault();
+        if (e.target.classList !== 0) {
+            if (e.target.classList.contains("square")) {
+                const x = parseInt(e.target.getAttribute("x"));
+                const y = parseInt(e.target.getAttribute("y"));
+
+                if (!board.matrix[x][y].visible) {
+                    if (e.target.textContent === flag) {
+                        e.target.textContent = "";
+                    } else {
+                        e.target.textContent = flag;
+                    }
+                }
+            }
+        }
     });
     
-  return (
-    <div className="App">
-      <div className="box">
-        {board.matrix.map((row, xIdx) => {
-          return (
-            row.map((square, yIdx) => {
-              return (<div onClick = {squareClicked} className="square" x = {xIdx} y={yIdx} text={square.text} id = {`${xIdx}${yIdx}`} key={uniqid()}></div>)
-            })
-          )
-        })}
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="box">
+                {board.matrix.map((row, xIdx) => {
+                    return row.map((square, yIdx) => {
+                        return (
+                            <div
+                                onClick={squareClicked}
+                                className="square"
+                                x={xIdx}
+                                y={yIdx}
+                                text={square.text}
+                                id={`${xIdx}${yIdx}`}
+                                key={uniqid()}
+                            ></div>
+                        );
+                    });
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
